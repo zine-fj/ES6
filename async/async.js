99c3f5550bcc83c781fbd8b9b571309a8cda963a
敏中 <747810974@qq.com>
2018-05-31 10:50:05 +0800

    async

--- async/async.js
+++ async/async.js
@@ -23,18 +23,9 @@ async function fn1() {
     let [a,b,c] = await Promise.all([
         readFile('data/a.txt'),
         readFile('data/b.txt'),
-        readFile('data/c.txt'),
     ]);
     console.log(a.toString());
     console.log(b.toString());
-    console.log(c.toString());
  }
  
  fn1();
-
-
-
-
-
-
-
